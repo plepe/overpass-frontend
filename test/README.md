3b7be8a418f176ece3aabd0b70e49ce39be214b2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-09-08 21:46:40 +0200

    Testdata

---
+++ test/README.md
@@ -0,0 +1,5 @@
+data.osm.bz2 is an excerpt of OpenStreetMap in the region of Vienna, Austria at 2016-09-08.
+
+Import this into an osm3s database with --meta enabled.
+
+Instead of setting up Apache2 with ExecCGI enabled, you can use the php script query.php.
---
+++ test/query.php
@@ -0,0 +1,29 @@
+<?php
+$descriptorspec = array(
+   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+   2 => array("pipe", "w") // stderr is a file to write to
+);
+
+$env = array();
+$pipes = array();
+$cwd = getcwd();
+
+$process = proc_open('osm3s_query --db-dir=data/', $descriptorspec, $pipes, $cwd, $env);
+
+if (is_resource($process)) {
+    // $pipes now looks like this:
+    // 0 => writeable handle connected to child stdin
+    // 1 => readable handle connected to child stdout
+    // Any error output will be appended to /tmp/error-output.txt
+
+    fwrite($pipes[0], file_get_contents("php://input"));
+    fclose($pipes[0]);
+
+    echo stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+
+    // It is important that you close any pipes before calling
+    // proc_close in order to avoid a deadlock
+    $return_value = proc_close($process);
+}
