b2e425d14cd4f156f2e47ab0fed00b1a14d72a25
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-10-12 22:31:52 +0200

    SortedCallbacks: if index is sequential, call featureCallback immediately

--- src/SortedCallbacks.js
+++ src/SortedCallbacks.js
@@ -2,8 +2,14 @@ var async = require('async')
 
 function SortedCallbacks (options, featureCallback, finalCallback) {
   this.list = []
+  this.lastIndex = -1
   this.featureCallback = featureCallback
   this.finalCallback = finalCallback
+  this.options = options
+
+  if (!('sort' in this.options)) {
+    this.options.sort = false
+  }
 }
 
 SortedCallbacks.prototype.next = function (err, feature, index) {
@@ -11,11 +17,20 @@ SortedCallbacks.prototype.next = function (err, feature, index) {
     err: err,
     feature: feature
   }
+
+  if ((this.options.sort === false) ||
+      ((this.options.sort === true) && (index === this.lastIndex + 1))) {
+    async.setImmediate(function () {
+      this.featureCallback(this.list[index].err, this.list[index].feature, index)
+    }.bind(this))
+
+    this.lastIndex = index
+  }
 }
 
 SortedCallbacks.prototype.final = function (err) {
   async.setImmediate(function () {
-    for (var i = 0; i < this.list.length; i++) {
+    for (var i = this.lastIndex + 1; i < this.list.length; i++) {
       this.featureCallback(this.list[i].err, this.list[i].feature, i)
     }
 
