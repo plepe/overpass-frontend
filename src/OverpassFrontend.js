5cd4cc03ca9b2a2d41e08b42b1e8b9eba538159b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-01-17 07:17:14 +0100

    RequestBBox.compileQuery()

--- src/OverpassFrontend.js
+++ src/OverpassFrontend.js
@@ -557,38 +557,11 @@ OverpassFrontend.prototype._processBBoxQuery = function (request) {
     }
   }
 
-  // var BBoxString = request.remainingBounds.toLatLonString()
-  // TODO: turf union/difference is broken - use full bounds instead
-  var BBoxString = request.bounds.toLatLonString()
-
-  var queryOptions = '[bbox:' + BBoxString + ']'
-
   var context = {
     request: request
   }
 
-  var query = '[out:json]' + queryOptions + ';\n(' + request.query + ')->.result;\n'
-
-  var queryRemoveDoneFeatures = ''
-  var countRemoveDoneFeatures = 0
-  for (var id in request.doneFeatures) {
-    var ob = request.doneFeatures[id]
-
-    if (countRemoveDoneFeatures % 1000 === 999) {
-      query += '(' + queryRemoveDoneFeatures + ')->.done;\n'
-      queryRemoveDoneFeatures = '.done;'
-    }
-
-    queryRemoveDoneFeatures += ob.type + '(' + ob.osm_id + ');'
-    countRemoveDoneFeatures++
-  }
-
-  if (countRemoveDoneFeatures) {
-    query += '(' + queryRemoveDoneFeatures + ')->.done;\n'
-    query += '(.result; - .done;);\n'
-  }
-
-  query += 'out ' + overpassOutOptions(request.options) + ';'
+  var query = request.compileQuery()
 
   setTimeout(function () {
     httpLoad(
