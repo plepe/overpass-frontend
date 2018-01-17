d582ca0b575f0b9b80eb1858173cf369d7acb677
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-01-17 07:12:24 +0100

    Move overpassOutOptions() to own file

---
+++ src/overpassOutOptions.js
@@ -0,0 +1,37 @@
+var defines = require('./defines')
+
+function overpassOutOptions (options) {
+  var outOptions = ''
+
+  if ('split' in options && options.split > 0) {
+    outOptions += options.split + ' '
+  }
+
+  if (options.properties & defines.META) {
+    outOptions += 'meta '
+  } else if (options.properties & defines.TAGS) {
+    if (options.properties & defines.MEMBERS) {
+      outOptions += 'body '
+    } else {
+      outOptions += 'tags '
+    }
+  } else if (options.properties & defines.MEMBERS) {
+    outOptions += 'skel '
+  } else {
+    outOptions += 'ids '
+  }
+
+  if (options.properties & defines.GEOM) {
+    outOptions += 'geom '
+  } else if (options.properties & defines.BBOX) {
+    outOptions += 'bb '
+  } else if (options.properties & defines.CENTER) {
+    outOptions += 'center '
+  }
+
+  outOptions += 'qt'
+
+  return outOptions
+}
+
+module.exports = overpassOutOptions
