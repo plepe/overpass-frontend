ec77c5876663beab4c6e76766d3f917027929d50
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-01-17 07:17:14 +0100

    RequestBBox.compileQuery()

--- src/RequestBBox.js
+++ src/RequestBBox.js
@@ -1,9 +1,42 @@
 const Request = require('./Request')
+const overpassOutOptions = require('./overpassOutOptions')
 
 class RequestBBox extends Request {
   constructor (overpass, data) {
     super(overpass, data)
     this.type = 'BBoxQuery'
+  }
+
+  compileQuery () {
+    // var BBoxString = this.remainingBounds.toLatLonString()
+    // TODO: turf union/difference is broken - use full bounds instead
+    var BBoxString = this.bounds.toLatLonString()
+    var queryOptions = '[bbox:' + BBoxString + ']'
+
+    var query = '[out:json]' + queryOptions + ';\n(' + this.query + ')->.result;\n'
+
+    var queryRemoveDoneFeatures = ''
+    var countRemoveDoneFeatures = 0
+    for (var id in this.doneFeatures) {
+      var ob = this.doneFeatures[id]
+
+      if (countRemoveDoneFeatures % 1000 === 999) {
+        query += '(' + queryRemoveDoneFeatures + ')->.done;\n'
+        queryRemoveDoneFeatures = '.done;'
+      }
+
+      queryRemoveDoneFeatures += ob.type + '(' + ob.osm_id + ');'
+      countRemoveDoneFeatures++
+    }
+
+    if (countRemoveDoneFeatures) {
+      query += '(' + queryRemoveDoneFeatures + ')->.done;\n'
+      query += '(.result; - .done);\n'
+    }
+
+    query += 'out ' + overpassOutOptions(this.options) + ';'
+
+    return query
   }
 }
 
