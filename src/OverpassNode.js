11f2c27577c51a36d89dd0a2f8a5bdf09a4e9e36
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-01-19 19:14:43 +0100

    Rewrite code as ES6 classes

--- src/OverpassNode.js
+++ src/OverpassNode.js
@@ -1,58 +1,54 @@
 /* global L:false */
 
-var util = require('util')
 var OverpassObject = require('./OverpassObject')
 var BoundingBox = require('boundingbox')
 
-util.inherits(OverpassNode, OverpassObject)
-function OverpassNode () {
-  OverpassObject.call(this)
-}
-
-OverpassNode.prototype.GeoJSON = function () {
-  return {
-    type: 'Feature',
-    id: this.type + '/' + this.osm_id,
-    geometry: {
-      type: 'Point',
-      coordinates: [ this.geometry.lon, this.geometry.lat ]
-    },
-    properties: this.GeoJSONProperties()
+class OverpassNode extends OverpassObject {
+  GeoJSON () {
+    return {
+      type: 'Feature',
+      id: this.type + '/' + this.osm_id,
+      geometry: {
+        type: 'Point',
+        coordinates: [ this.geometry.lon, this.geometry.lat ]
+      },
+      properties: this.GeoJSONProperties()
+    }
   }
-}
 
-OverpassNode.prototype.updateData = function (data, options) {
-  if (data.lat) {
-    this.geometry = {
-      lat: data.lat,
-      lon: data.lon
+  updateData (data, options) {
+    if (data.lat) {
+      this.geometry = {
+        lat: data.lat,
+        lon: data.lon
+      }
+
+      this.bounds = new BoundingBox(data)
+      this.center = this.bounds.getCenter()
     }
 
-    this.bounds = new BoundingBox(data)
-    this.center = this.bounds.getCenter()
+    super.updateData(data, options)
   }
 
-  this.constructor.super_.prototype.updateData.call(this, data, options)
-}
-
-OverpassNode.prototype.leafletFeature = function (options) {
-  switch ('nodeFeature' in options ? options.nodeFeature : null) {
-    case 'Marker':
-      return L.marker(this.geometry, options)
-    case 'Circle':
-      return L.circle(this.geometry, options.radius, options)
-    case 'CircleMarker':
-    default:
-      return L.circleMarker(this.geometry, options)
-  }
-}
-
-OverpassNode.prototype.intersects = function (bbox) {
-  if (!this.bounds) {
-    return 1
+  leafletFeature (options) {
+    switch ('nodeFeature' in options ? options.nodeFeature : null) {
+      case 'Marker':
+        return L.marker(this.geometry, options)
+      case 'Circle':
+        return L.circle(this.geometry, options.radius, options)
+      case 'CircleMarker':
+      default:
+        return L.circleMarker(this.geometry, options)
+    }
   }
 
-  return bbox.intersects(this.bounds) ? 2 : 0
+  intersects (bbox) {
+    if (!this.bounds) {
+      return 1
+    }
+
+    return bbox.intersects(this.bounds) ? 2 : 0
+  }
 }
 
 module.exports = OverpassNode
