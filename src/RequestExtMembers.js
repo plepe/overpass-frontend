dae89a1303780f11784b3a273c34a6bdbb498c40
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-02-17 16:55:12 +0100

    RequestExtMembers: complete rewrite: class, query by relation id

--- src/RequestExtMembers.js
+++ src/RequestExtMembers.js
@@ -1,168 +1,166 @@
 const defines = require('./defines')
 const overpassOutOptions = require('./overpassOutOptions')
-const Quadtree = require('quadtree-lookup')
-const toQuadtreeLookupBox = require('./toQuadtreeLookupBox')
-const turf = require('./turf')
-const BoundingBox = require('boundingbox')
 const each = require('lodash/forEach')
 const map = require('lodash/map')
 const keys = require('lodash/keys')
 
-function requestExtMembersInit () {
-  this.options.properties |= defines.MEMBERS
+class RequestExtMembers {
+  constructor (request) {
+    this.master = request
+    this.options = this.master.options
+    this.overpass = this.master.overpass
 
-  this.doneFeaturesExtMembers = {}
-  this.todoExtMembers = {}
-}
+    this.options.properties |= defines.MEMBERS
+    this.options.memberProperties = this.options.memberProperties || defines.DEFAULT
+    this.options.memberProperties |= defines.BBOX
 
-function requestExtMembersPreprocess (fun) {
-  fun.call(this)
+    this.master.compileQuery = this.compileQuery.bind(this, this.master.compileQuery)
+    this.master.needLoad = this.needLoad.bind(this, this.master.needLoad)
+    this.master.preprocess = this.preprocess.bind(this, this.master.preprocess)
+    this.master.willInclude = this.willInclude.bind(this, this.master.willInclude)
+    this.master.finishSubRequest = this.finishSubRequest.bind(this, this.master.finishSubRequest)
+    this.master.featureCallback = this.receiveMasterObject.bind(this, this.master.featureCallback)
 
-  this.todoExtMembers = {}
-  each(this.doneFeatures, ob => {
-    each(ob.members, member => {
-      if (!(member.id in this.doneFeaturesExtMembers)) {
-        this.todoExtMembers[member.id] = undefined
+    this.doneFeatures = {}
+    this.relations = {}
+    this.currentRelations = []
+    this.todo = {}
+  }
+
+  willInclude (fun, context) {
+    let result = fun.call(this.master, context)
+
+    if (!this.loadFinish) {
+      return true
+    }
+
+    return result
+  }
+
+  preprocess (fun) {
+    fun.call(this.master)
+
+    this.todo = {}
+    each(this.relations, ob => {
+      each(ob.members, member => {
+        if (!(member.id in this.doneFeatures)) {
+          this.todo[member.id] = undefined
+        }
+      })
+    })
+
+    each(this.todo, (value, id) => {
+      if (id in this.overpass.cacheElements) {
+        var ob = this.overpass.cacheElements[id]
+
+        if (!ob.intersects(this.master.bounds)) {
+          return
+        }
+
+        if ((this.options.memberProperties & ob.properties) === this.options.memberProperties) {
+          this.doneFeatures[id] = ob
+
+          this.options.memberCallback(null, ob)
+        }
       }
     })
-  })
 
-  each(this.todoExtMembers, (value, id) => {
-    if (id in this.overpass.cacheElements) {
-    }
-  })
-
-  return
-
-
-  var quadtreeBounds = toQuadtreeLookupBox(this.bounds)
-
-  var items = this.cacheExtMembers.elements.queryRange(quadtreeBounds)
-  // TODO: do something with 'items'
-
-  for (var i = 0; i < items.length; i++) {
-    var id = items[i].value
-    var ob = this.overpass.cacheElements[id]
-
-    if (id in this.doneFeaturesExtMembers) {
-      continue
-    }
-
-    // also check the object directly if it intersects the bbox - if possible
-    if (!ob.intersects(this.bounds)) {
-      continue
-    }
-
-    if ((this.options.memberProperties & ob.properties) === this.options.memberProperties) {
-      this.doneFeaturesExtMembers[id] = ob
-
-      this.options.memberCallback(null, ob)
-    }
+    return
   }
-}
 
-function requestExtMembersCompileQuery (fun, context) {
-  var subRequest = fun.call(this, context)
+  compileQuery (fun, context) {
+    var subRequest = fun.call(this.master, context)
 
-  if (keys(this.doneFeatures).length === 0) {
+    if (keys(this.relations).length === 0) {
+      return subRequest
+    }
+
+    let query = '(\n'
+    query += map(this.relations, ob => {
+      if (ob.type === 'relation') {
+        return 'relation(' + ob.osm_id + ');\n'
+      }
+      return ''
+    }).join('')
+    query += ')->.result;'
+    this.currentRelations = keys(this.relations)
+
+    let BBoxString = this.master.bounds.toLatLonString()
+    query += '(\n' +
+       '  node(r.result)(' + BBoxString + ');\n' +
+       '  way(r.result)(' + BBoxString + ');\n' +
+       '  relation(r.result)(' + BBoxString + ');\n' +
+       ')->.resultMembers;\n'
+
+    var queryRemoveDoneFeatures = ''
+    var countRemoveDoneFeatures = 0
+    for (var id in this.doneFeatures) {
+      var ob = this.doneFeatures[id]
+
+      if (countRemoveDoneFeatures % 1000 === 999) {
+        query += '(' + queryRemoveDoneFeatures + ')->.doneMembers;\n'
+        queryRemoveDoneFeatures = '.doneMembers;'
+      }
+
+      queryRemoveDoneFeatures += ob.type + '(' + ob.osm_id + ');'
+      countRemoveDoneFeatures++
+    }
+
+    if (countRemoveDoneFeatures) {
+      query += '(' + queryRemoveDoneFeatures + ')->.doneMembers;\n'
+      query += '(.resultMembers; - .doneMembers);\n'
+    } else {
+      query += '(.resultMembers);\n'
+    }
+
+    this.part = {
+      properties: this.options.memberProperties,
+      receiveObject: this.receiveObject.bind(this),
+      featureCallback: this.options.memberCallback,
+      count: 0
+    }
+
+    query += 'out ' + overpassOutOptions(this.part) + ';'
+
+    this.loadFinish = true
+
+    if (subRequest.parts.length) {
+      subRequest.query += '\nout count;\n'
+    }
+    subRequest.query += query
+    subRequest.parts.push(this.part)
+
     return subRequest
   }
 
-  let query = '(\n'
-  map(this.doneFeatures, ob => {
-    if (ob.type === 'relation') {
-      return 'relation(' + ob.id + ');\n';
+  receiveMasterObject (fun, err, result, index) {
+    this.relations[result.id] = result
+    fun(err, result, index)
+  }
+
+  receiveObject (ob) {
+    this.doneFeatures[ob.id] = ob
+  }
+
+  finishSubRequest (fun, subRequest) {
+    fun.call(this.master, subRequest)
+
+    if (keys(this.relations).length !== this.currentRelations.length) {
+      this.loadFinish = false
     }
-    return ''
-  }).join('')
-  query += ')->.result;'
-
-  let BBoxString = this.bounds.toLatLonString()
-  query = '(\n' +
-     '  node(r.result)(' + BBoxString + ');\n' +
-     '  way(r.result)(' + BBoxString + ');\n' +
-     '  relation(r.result)(' + BBoxString + ');\n' +
-     ')->.resultMembers;\n'
-
-  var queryRemoveDoneFeatures = ''
-  var countRemoveDoneFeatures = 0
-  for (var id in this.doneFeaturesExtMembers) {
-    var ob = this.doneFeaturesExtMembers[id]
-
-    if (countRemoveDoneFeatures % 1000 === 999) {
-      query += '(' + queryRemoveDoneFeatures + ')->.doneMembers;\n'
-      queryRemoveDoneFeatures = '.doneMembers;'
-    }
-
-    queryRemoveDoneFeatures += ob.type + '(' + ob.osm_id + ');'
-    countRemoveDoneFeatures++
   }
 
-  if (countRemoveDoneFeatures) {
-    query += '(' + queryRemoveDoneFeatures + ')->.doneMembers;\n'
-    query += '(.resultMembers; - .doneMembers);\n'
-  } else {
-    query += '(.resultMembers);\n'
-  }
+  needLoad (fun) {
+    var result = fun.call(this.master)
 
-  var membersOptions = {
-    split: this.options.memberSplit,
-    effortSplit: this.options.membersEffortSplit,
-    properties: this.options.memberProperties,
-    receiveObject: requestExtMembersReceive.bind(this),
-    featureCallback: this.options.memberCallback,
-    count: 0
-  }
-
-  query += 'out ' + overpassOutOptions(membersOptions) + ';'
-
-  subRequest.query += '\nout count;\n' + query
-  subRequest.parts.push(membersOptions)
-  this.subRequestIndexExtMembers = 
-
-  return subRequest
-}
-
-function requestExtMembersReceive (ob) {
-  this.doneFeaturesExtMembers[ob.id] = ob
-}
-
-function requestExtMembersFinishSubRequest (subRequest) {
-  var result = fun.call(this)
-
-  if (('membersEffortSplit' in this.options && this.options.membersEffortSplit > subRequest.parts[].count) ||
-      (this.options.split > subRequest.parts[0].count)) {
-    this.loadFinish = true
-
-}
-
-function requestExtMembersNeedLoad (fun) {
-  var result = fun.call(this)
-  return result
-
-  if (result === true) {
-    return true
-  }
-
-  if (this.cacheExtMembers.requested !== null) {
-    var toRequest = this.bounds.toGeoJSON()
-    remainingBounds = turf.difference(toRequest, this.cacheExtMembers.requested)
-
-    if (remainingBounds === undefined) {
-      return false
-    } else {
-      this.remainingBoundsExtMembers = new BoundingBox(remainingBounds)
+    if (result === true) {
       return true
     }
-  }
 
-  return false
+    return !this.loadFinish
+  }
 }
 
 module.exports = function (request) {
-  request.compileQuery = requestExtMembersCompileQuery.bind(request, request.compileQuery)
-  request.needLoad = requestExtMembersNeedLoad.bind(request, request.needLoad)
-  request.preprocess = requestExtMembersPreprocess.bind(request, request.preprocess)
-
-  requestExtMembersInit.call(request)
+  return new RequestExtMembers(request)
 }
