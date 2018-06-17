9d387b865897a1e5e7bb501acc55acd0e90eeceb
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-06-17 10:04:36 +0200

    knownArea: remove debug message

--- src/knownArea.js
+++ src/knownArea.js
@@ -34,7 +34,7 @@ class KnownArea {
     bbox = new BoundingBox(bbox).toGeoJSON()
 
     let remaining = turf.difference(bbox, this.area)
-    console.log(JSON.stringify(this.area), JSON.stringify(bbox), !remaining)
+    // console.log(JSON.stringify(this.area), JSON.stringify(bbox), !remaining)
 
     return !remaining
   }
