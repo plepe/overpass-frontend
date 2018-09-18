81c8f027ddc1ada3575b4e3881700254da192f6b
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-09-18 14:06:56 +0200

    unfin

--- test/loadOsmFile.js
+++ test/loadOsmFile.js
@@ -1,13 +1,26 @@
 const loadOsmFile = require('../src/loadOsmFile')
 if (typeof XMLHttpRequest === 'undefined') {
   global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
-  const DOMParser = require('xmldom').DOMParser
 }
+
+const lokijs = require('lokijs')
 
 describe('.osm', function () {
   it ('test', function (done) {
     loadOsmFile('test/data.osm', (err, result) => {
-      console.log(result)
+
+      var db = new lokijs()
+      let osm = db.addCollection('osm')
+      osm.insert(result.elements.map(ob => ob.tags))
+
+
+      console.log(osm.find({ 'cuisine': { '$contains': 'kebab' }}))
+
+
+      console.log(result)//.map(ob => ob.tags))
+
+
+
       done(err)
     })
   })
