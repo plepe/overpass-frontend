f1dd316ce9e9d584d1b49c3ee9f9d3bdb06265fc
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2018-09-28 13:59:43 +0200

    osm3sMeta: Copy data from results

--- src/OverpassFrontend.js
+++ src/OverpassFrontend.js
@@ -249,11 +249,14 @@ class OverpassFrontend {
       this.errorCount = 0
     }
 
-    let osm3sMeta = {
-      osm3s_version: results.version,
-      generator: results.generator,
-      osm_timestamp: results.osm3s.timestamp_osm_base,
-      copyright: results.osm3s.copyright
+    let osm3sMeta = {}
+    for (let k in results) {
+      if (k !== 'elements' && k !== 'osm3s') {
+        osm3sMeta[k] = results[k]
+      }
+    }
+    for (let k in results.osm3s) {
+      osm3sMeta[k] = results.osm3s[k]
     }
 
     var subRequestsIndex = 0
