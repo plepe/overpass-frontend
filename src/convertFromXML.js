ac4ace6b7994036700dd9b59c38d4f2fb5c2810b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2018-09-29 19:16:20 +0200

    Load files: convert xml format to json format + tests

--- src/convertFromXML.js
+++ src/convertFromXML.js
@@ -1,6 +1,17 @@
 module.exports = function convertFromXML (xml) {
   let result = {
+    version: parseFloat(xml.getAttribute('version')),
+    generator: xml.getAttribute('generator'),
+    osm3s: {
+      copyright: xml.getElementsByTagName('note')[0].textContent
+    },
     elements: []
+  }
+
+  let meta = xml.getElementsByTagName('meta')[0]
+  result.osm3s['timestamp_osm_base'] = meta.getAttribute('osm_base')
+  if (meta.hasAttribute('areas')) {
+    result.osm3s['timestamp_areas_base'] = meta.getAttribute('areas')
   }
 
   let current = xml.firstChild
@@ -9,12 +20,16 @@ module.exports = function convertFromXML (xml) {
 
     if (current.nodeName === 'node' || current.nodeName === 'way' || current.nodeName === 'relation') {
       element.type = current.nodeName
-      element.id = current.getAttribute('id')
-      element.tags = {}
+      element.id = parseInt(current.getAttribute('id'))
+      element.changeset = parseInt(current.getAttribute('changeset'))
+      element.timestamp = current.getAttribute('timestamp')
+      element.user = current.getAttribute('user')
+      element.uid = parseInt(current.getAttribute('uid'))
+      element.version = parseInt(current.getAttribute('version'))
 
       if (element.type === 'node') {
-        element.lat = current.getAttribute('lat')
-        element.lon = current.getAttribute('lon')
+        element.lat = parseFloat(current.getAttribute('lat'))
+        element.lon = parseFloat(current.getAttribute('lon'))
       } else if (element.type === 'way') {
         element.nodes = []
       } else if (element.type === 'relation') {
@@ -24,15 +39,19 @@ module.exports = function convertFromXML (xml) {
       let child = current.firstChild
       while (child) {
         if (child.nodeName === 'tag') {
+          if (!('tags' in element)) {
+            element.tags = {}
+          }
+
           element.tags[child.getAttribute('k')] = child.getAttribute('v')
         } else if (child.nodeName === 'member') {
           element.members.push({
             type: child.getAttribute('type'),
-            ref: child.getAttribute('ref'),
+            ref: parseInt(child.getAttribute('ref')),
             role: child.getAttribute('role')
           })
         } else if (child.nodeName === 'nd') {
-          element.nodes.push(child.getAttribute('ref'))
+          element.nodes.push(parseInt(child.getAttribute('ref')))
         }
 
         child = child.nextSibling
--- test/loadOsmFile.js
+++ test/loadOsmFile.js
@@ -1,14 +1,43 @@
+var assert = require('assert')
+
 const loadOsmFile = require('../src/loadOsmFile')
 if (typeof XMLHttpRequest === 'undefined') {
   global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
   const DOMParser = require('xmldom').DOMParser
 }
 
-describe('.osm', function () {
-  it ('test', function (done) {
-    loadOsmFile('test/data.osm.bz2', (err, result) => {
-      console.log(result)
+var parsedData
+
+describe('Load OSM data from file', function () {
+  it ('.json', function (done) {
+    loadOsmFile('test/small.json', (err, result) => {
+      parsedData = result
+
       done(err)
+    })
+  })
+
+  it ('.osm', function (done) {
+    loadOsmFile('test/small.osm', (err, result) => {
+      if (err) {
+        return done(err)
+      }
+
+      assert.deepEqual(result, parsedData)
+
+      done()
+    })
+  })
+
+  it ('.osm.bz2', function (done) {
+    loadOsmFile('test/small.osm.bz2', (err, result) => {
+      if (err) {
+        return done(err)
+      }
+
+      assert.deepStrictEqual(result, parsedData)
+
+      done()
     })
   })
 })
