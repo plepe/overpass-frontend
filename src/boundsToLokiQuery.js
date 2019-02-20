e9c27e71093fd2d2faa9ac6190c5804f3dbb6a05
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2019-02-20 12:06:37 +0100

    boundsToLokiQuery: adapt query to use stretchLon180 property

--- src/boundsToLokiQuery.js
+++ src/boundsToLokiQuery.js
@@ -1,26 +1,39 @@
 module.exports = function boundsToLokiQuery (bounds) {
   if (bounds.minlon <= bounds.maxlon) {
     return {
-      minlat: { $lte: bounds.maxlat },
-      minlon: { $lte: bounds.maxlon },
-      maxlat: { $gte: bounds.minlat },
-      maxlon: { $gte: bounds.minlon }
+      $or: [ {
+        minlat: { $lte: bounds.maxlat },
+        minlon: { $lte: bounds.maxlon },
+        maxlat: { $gte: bounds.minlat },
+        maxlon: { $gte: bounds.minlon }
+      }, {
+        minlat: { $lte: bounds.maxlat },
+        maxlat: { $gte: bounds.minlat },
+        minlon: { $lte: bounds.maxlon },
+        stretchLon180: { $eq: true }
+      }, {
+        minlat: { $lte: bounds.maxlat },
+        maxlat: { $gte: bounds.minlat },
+        maxlon: { $gte: bounds.minlon },
+        stretchLon180: { $eq: true }
+      } ]
     }
   } else {
     return {
       $or: [ {
         minlat: { $lte: bounds.maxlat },
         maxlat: { $gte: bounds.minlat },
+        minlon: { $gte: 0 },
         maxlon: { $gte: bounds.minlon }
       }, {
         minlat: { $lte: bounds.maxlat },
         maxlat: { $gte: bounds.minlat },
-        minlon: { $lte: bounds.maxlon }
+        minlon: { $lte: bounds.maxlon },
+        maxlon: { $lte: 0 }
       }, {
         minlat: { $lte: bounds.maxlat },
         maxlat: { $gte: bounds.minlat },
-        minlon: { $lte: 180 },
-        maxlon: { $gte: -180 }
+        stretchLon180: { $eq: true }
       } ]
     }
   }
