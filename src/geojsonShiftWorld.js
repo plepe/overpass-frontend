571c8b235b4578863f4679b535317f58faa1535a
Stephan Bösch-Plepelits <skunk@cg.tuwien.ac.at>
2019-02-20 10:43:46 +0100

    geojsonShiftWorld: other feature types

--- src/geojsonShiftWorld.js
+++ src/geojsonShiftWorld.js
@@ -11,10 +11,33 @@ function geojsonShiftWorld (geojson, shift) {
         geometry: geojsonShiftWorld(geojson.geometry, shift),
         properties: geojson.properties
       }
-    case 'Polygon':
+    case 'GeometryCollection':
       return {
-        type: 'Polygon',
+        type: 'GeometryCollection',
+        geometries: geojson.geometries.map(geometry => geojsonShiftWorld(geometry, shift)),
+        properties: geojson.properties
+      }
+    case 'Point':
+      return {
+        type: 'Point',
+        coordinates: [ geojson.coordinates[0] + shift[geojson.coordinates[0] < 0 ? 0 : 1], geojson.coordinates[1] ]
+      }
+    case 'Polygon':
+    case 'MultiLineString':
+      return {
+        type: geojson.type,
         coordinates: geojson.coordinates.map(ring => ring.map(poi => [ poi[0] + shift[poi[0] < 0 ? 0 : 1], poi[1] ]))
+      }
+    case 'MultiPolygon':
+      return {
+        type: 'MultiPolygon',
+        coordinates: geojson.coordinates.map(poly => poly.map(ring => ring.map(poi => [ poi[0] + shift[poi[0] < 0 ? 0 : 1], poi[1] ])))
+      }
+    case 'LineString':
+    case 'MultiPoint':
+      return {
+        type: geojson.type,
+        coordinates: geojson.coordinates.map(poi => [ poi[0] + shift[poi[0] < 0 ? 0 : 1], poi[1] ])
       }
     default:
       console.log(geojson)
