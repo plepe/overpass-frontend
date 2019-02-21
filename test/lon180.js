8fe3fd803523ab97ccae3756e7d89ebcda8887de
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2019-02-21 21:40:34 +0100

    Test lon180: more tests

--- test/lon180.js
+++ test/lon180.js
@@ -140,4 +140,125 @@ describe('Lon180 (query objects near lon180)', function() {
 
     request.on('subrequest-compile', compileListener)
   })
+
+  it('Western part (fully cached)', function (done) {
+    var finalCalled = 0
+    var expected = [ 'w660439767', 'r3237099' ]
+    var found = []
+    var error = ''
+
+    var expectedSubRequestCount = 0
+    var foundSubRequestCount = 0
+
+    function compileListener (subrequest) {
+      foundSubRequestCount++
+    }
+
+    let request = overpassFrontend.BBoxQuery(
+      "nwr[natural=water]",
+      {
+	"minlat": 65,
+	"maxlat": 66,
+	"minlon": 179,
+	"maxlon": 180
+      },
+      {
+          properties: OverpassFrontend.MEMBERS | OverpassFrontend.GEOM | OverpassFrontend.TAGS
+      },
+      function (err, result) {
+        found.push(result.id)
+
+        if (expected.indexOf(result.id) === -1) {
+          error += 'Unexpected result ' + result.id + '\n'
+        }
+      },
+      function (err) {
+        assert.equal(finalCalled++, 0, 'Final function called ' + finalCalled + ' times!')
+        if (err) {
+          return done(err)
+        }
+
+        if (error) {
+          return done(error)
+        }
+
+        if (found.length !== expected.length) {
+          return done('Wrong count of objects returned:\n' +
+               'Expected: ' + expected.join(', ') + '\n' +
+               'Found: ' + found.join(', '))
+        }
+
+        assert.equal(overpassFrontend.hasStretchLon180, true)
+
+        assert.equal(foundSubRequestCount, expectedSubRequestCount, 'Wrong count of sub requests!')
+
+        request.off('subrequest-compile', compileListener)
+
+        done()
+      }
+    )
+
+    request.on('subrequest-compile', compileListener)
+  })
+
+  it('Eastern part (fully cached)', function (done) {
+    var finalCalled = 0
+    var expected = [ 'r3237099', 'w62065034', 'w62065032' ]
+    var found = []
+    var error = ''
+
+    var expectedSubRequestCount = 0
+    var foundSubRequestCount = 0
+
+    function compileListener (subrequest) {
+      foundSubRequestCount++
+    }
+
+    let request = overpassFrontend.BBoxQuery(
+      "nwr[natural=water]",
+      {
+	"minlat": 65,
+	"maxlat": 66,
+	"minlon": -180,
+	"maxlon": -179
+      },
+      {
+          properties: OverpassFrontend.MEMBERS | OverpassFrontend.GEOM | OverpassFrontend.TAGS
+      },
+      function (err, result) {
+        found.push(result.id)
+
+        if (expected.indexOf(result.id) === -1) {
+          error += 'Unexpected result ' + result.id + '\n'
+        }
+      },
+      function (err) {
+        assert.equal(finalCalled++, 0, 'Final function called ' + finalCalled + ' times!')
+        if (err) {
+          return done(err)
+        }
+
+        if (error) {
+          return done(error)
+        }
+
+        if (found.length !== expected.length) {
+          return done('Wrong count of objects returned:\n' +
+               'Expected: ' + expected.join(', ') + '\n' +
+               'Found: ' + found.join(', '))
+        }
+
+        assert.equal(overpassFrontend.hasStretchLon180, true)
+
+        assert.equal(foundSubRequestCount, expectedSubRequestCount, 'Wrong count of sub requests!')
+
+        request.off('subrequest-compile', compileListener)
+
+        done()
+      }
+    )
+
+    request.on('subrequest-compile', compileListener)
+  })
+
 })
