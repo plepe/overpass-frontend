d96789df08bda4ec6f908949029f60bcc614efa9
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-12-24 22:24:56 +0100

    OverpassRelation.intersect(): support geojson for geom/bounds crossing lon180

---
+++ src/booleanWithin.js
@@ -0,0 +1,28 @@
+const turf = {
+  booleanWithin: require('@turf/boolean-within').default
+}
+
+module.exports = function booleanWithin (feature1, feature2) {
+  const features1 = splitGeometry(feature1)
+  const features2 = splitGeometry(feature2)
+
+  const matching = features1.filter(f1 => !!features2.filter(f2 => turf.booleanWithin(f1, f2)).length)
+
+  return features1.length === matching.length
+}
+
+function splitGeometry (feature) {
+  if (feature.geometry && feature.geometry.type === 'MultiPolygon') {
+    return feature.geometry.coordinates.map(polygon => {
+      return {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: polygon
+        }
+      }
+    })
+  }
+
+  return [feature]
+}
