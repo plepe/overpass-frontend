44578c194c935f76e7f6e8556731cd176367d955
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2021-12-27 17:07:28 +0100

    Update documentation

--- src/OverpassFrontend.js
+++ src/OverpassFrontend.js
@@ -207,6 +207,10 @@ class OverpassFrontend {
    * @param {string|boolean} [options.sort=false] - When set to true or "index", the function featureCallback will be called in order of the "ids" array. When set to false or null, the featureCallback will be called as soon as the object is loaded (e.g. immediately, if it is cached). When set to "BBoxDiagonalLength", the objects are ordered by the length of the diagonal of the bounding box.
    * @param {"asc"|"desc"} [options.sortDir="asc"] Sort direction.
    * @param {BoundingBox|GeoJSON} [options.bounds] - Only return items which intersect these bounds. Boundaries is a BoundingBox, or a Leaflet Bounds object (e.g. from map.getBounds()) or a GeoJSON Polygon/Multipolygon.
+   * @param {boolean} [options.members=false] Query relation members of. Default: false
+   * @param {function} [options.memberCallback] For every member, call this callback function. (Requires options.members=true)
+   * @param {bit_array} [options.memberProperties] Which properties should be loaded for the members. Default: OverpassFrontend.TAGS | OverpassFrontend.MEMBERS | OverpassFrontend.BBOX
+   * @param {BoundingBox|GeoJSON} [options.memberBounds] - Only return members which intersect these bounds. Boundaries is a BoundingBox, or a Leaflet Bounds object (e.g. from map.getBounds()) or a GeoJSON Polygon/Multipolygon.
    * @param {function} featureCallback Will be called for each object which is passed in parameter 'ids'. Will be passed: 1. err (if an error occured, otherwise null), 2. the object or null, 3. index of the item in parameter ids.
    * @param {function} finalCallback Will be called after the last feature. Will be passed: 1. err (if an error occured, otherwise null).
    * @return {RequestGet}
@@ -555,6 +559,7 @@ class OverpassFrontend {
    * @param {boolean} [options.members=false] Query relation members of. Default: false
    * @param {function} [options.memberCallback] For every member, call this callback function. (Requires options.members=true)
    * @param {bit_array} [options.memberProperties] Which properties should be loaded for the members. Default: OverpassFrontend.TAGS | OverpassFrontend.MEMBERS | OverpassFrontend.BBOX
+   * @param {BoundingBox|GeoJSON} [options.memberBounds] - Only return members which intersect these bounds. Boundaries is a BoundingBox, or a Leaflet Bounds object (e.g. from map.getBounds()) or a GeoJSON Polygon/Multipolygon.
    * @param {number} [options.memberSplit=0] If more than 'memberSplit' member elements would be returned, split into smaller requests (see 'split'). 0 = do not split.
    * @param {string|Filter} [options.filter] Additional filter.
    * @param {boolean} [options.noCacheQuery=false] If true, the local cache will not be queried
