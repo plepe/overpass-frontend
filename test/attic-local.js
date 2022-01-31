d354ff45a7c57201691339efec8f805a71b8f6c2
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-01-31 21:53:41 +0100

    Attic data, BBoxQuery ways: add tests (with error)

--- test/attic-local.js
+++ test/attic-local.js
@@ -164,4 +164,75 @@ describe('Attic data from local file', function () {
       )
   })
 
+  it('Load ways (buildings) by bbox at different timestamps', function (done) {
+    const bbox = {
+      minlat: 48.20200,
+      maxlat: 48.20300,
+      minlon: 16.34000,
+      maxlon: 16.34100
+    }
+    const timestamps = ['2009-01-01T00:00:00Z', '2012-01-01T00:00:00Z', '2013-01-01T00:00:00Z', '2018-02-01T00:00:00Z', '2020-01-01T00:00:00Z', null]
+    const expectedTimestamps = {
+      'w86127673': [null, '2011-07-23T20:34:07Z', null, null, null, '2021-04-04T17:48:07Z'], // TODO: last index should be null
+      'w86127644': [null, '2011-07-23T20:34:06Z', null, null, null, null],
+      'w86124803': [null, '2011-07-23T20:34:01Z', null, null, null, null],
+      'w123386238': [null, '2011-07-27T19:47:00Z', '2012-04-29T01:42:37Z', '2012-04-29T01:42:37Z', '2012-04-29T01:42:37Z', '2021-12-30T19:51:29Z'],
+      'w161573351': [null, null, '2012-04-29T17:02:28Z', '2013-02-11T05:27:18Z', '2013-02-11T05:27:18Z', '2013-02-11T05:27:18Z'],
+      'w161573356': [null, null, '2012-04-29T17:02:29Z', '2013-02-11T05:27:18Z', '2013-02-11T05:27:18Z', '2013-02-11T05:27:18Z'],
+      'w161573365': [null, null, '2012-04-29T17:02:32Z', '2012-04-29T17:02:32Z', '2012-04-29T17:02:32Z', '2012-04-29T17:02:32Z'],
+      'w161604906': [null, null, '2012-04-29T21:50:42Z', '2012-04-29T21:50:42Z', '2012-04-29T21:50:42Z', '2021-05-19T18:46:10Z'],
+      'w161610222': [null, null, '2012-04-29T22:41:31Z', '2012-04-29T22:41:31Z', '2012-04-29T22:41:31Z', '2012-04-29T22:41:31Z'],
+      'w161573360': [null, null, '2012-04-29T17:02:30Z', '2012-04-29T17:02:30Z', '2012-04-29T17:02:30Z', '2021-03-15T18:13:35Z'],
+    }
+    const expectedMemberVersions = {
+      "w86127673": [ "", "2,2,2,2,1,2", "", "", "", "4,1,1,1,1,1,1,2,4,3,1,4,1,1,4" ], // TODO: last index should be ""
+      "w86127644": [ "", "2,2,2,2,2", "", "", "", "" ],
+      "w86124803": [ "", "2,1,2,2,1,2,2", "", "", "", "" ],
+      "w123386238": [ "", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "2,2,2,2,2,2,2,2,2,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,2,2,2,1,2", "2,2,2,2,2,2,2,2,2,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,2,2,2,1,2", "2,2,2,2,2,2,2,2,2,1,1,1,1,2,2,2,2,2,2,1,1,1,1,1,2,2,2,1,2", "2,2,2,2,2,2,2,2,2,1,1,1,1,2,2,2,1,2,2,2,1,1,1,1,1,2,2,2,1,2" ],
+      "w161573351": [ "", "", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1" ],
+      "w161573356": [ "", "", "1,1,1,1,1,1", "1,1,1,1,1,1", "1,1,1,1,1,1", "1,1,1,1,1,1" ],
+      "w161573365": [ "", "", "1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1" ],
+      "w161604906": [ "", "", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1" ],
+      "w161610222": [ "", "", "2,1,1,1,1,1,1,1,1,1,4,1,1,2", "2,1,1,1,1,1,1,1,1,1,4,1,1,2", "2,1,1,1,1,1,1,1,1,1,4,1,1,2", "2,1,1,1,1,1,1,1,1,1,4,1,1,2" ],
+      "w161573360": [ "", "", "1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1", "1,1,1,1,1,1,1,1,1,1" ]
+    }
+
+    async.eachOf(timestamps,
+      (date, i, done) => {
+        const found = []
+        const expected = Object.keys(expectedTimestamps).filter(e => expectedTimestamps[e][i])
+
+        overpassFrontend.BBoxQuery(
+          'way[building]', bbox,
+          { date },
+          function (err, result) {
+            found.push(result.id)
+
+            console.log('At ' + date + ' found:', result.id, 'with ts', result.meta.timestamp)
+
+            if (expected.indexOf(result.id) === -1) {
+              assert.fail('At ' + date + ', unexpected result ' + result.id + ' (ts ' + result.meta.timestamp + ')')
+            }
+            assert.equal(result.meta.timestamp, expectedTimestamps[result.id][i], result.id + ' at date ' + date + ' has wrong timestamp')
+
+            const memberVersions = result.memberObjects().map(node => node ? node.meta.version : '').join(',')
+            assert.equal(memberVersions, expectedMemberVersions[result.id][i], result.id + ' at date ' + date + ' has wrong member versions')
+          },
+          function (err) {
+            if (err) {
+              return done(err)
+            }
+
+
+            if (found.length !== expected.length) {
+              return done('At date ' + date + ', wrong count of objects returned:\n' +
+               'Expected: ' + expected.join(', ') + '\n' +
+               'Found: ' + found.join(', '))
+            }
+
+            done()
+          })
+        }, done
+      )
+  })
 })
