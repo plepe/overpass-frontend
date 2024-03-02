b383b07040b9dc7d0cd55cf0c4ccb28f4ebd8a54
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-03-02 07:47:10 +0100

    isFileURL: revert logic - try to detect server urls instead

--- src/isFileURL.js
+++ src/isFileURL.js
@@ -1,3 +1,15 @@
 module.exports = (url) => {
-  return url.match(/\.(json|osm\.bz2|osm)$/)
+  if (!url || url.match(/\/\/[^\/]+$/)) { // url without path
+    return false
+  }
+
+  if (url.match(/\/[^\/.]+$/)) { // last url part without '.'
+    return false
+  }
+
+  if (url.match(/\.(php)(\?.*)?$/)) { // php script -> assume server
+    return false
+  }
+
+  return true
 }
