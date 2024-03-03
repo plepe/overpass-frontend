a890e4f4cd5596abcaf031720b3589e958cd8a9b
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2024-03-03 07:49:04 +0100

    Re-create loadOsmFile.js test as fileFormats.js

---
+++ test/fileFormats.js
@@ -0,0 +1,50 @@
+var assert = require('assert')
+
+const loadFile = require('../src/loadFile')
+const fileFormatOSMJSON = require('../src/fileFormatOSMJSON')
+const fileFormatOSMXML = require('../src/fileFormatOSMXML')
+
+if (typeof XMLHttpRequest === 'undefined') {
+  global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+  const DOMParser = require('@xmldom/xmldom').DOMParser
+}
+
+describe('Load OSM data from file', function () {
+  it ('.json', function (done) {
+    loadFile('test/small.json', (err, content) => {
+      fileFormatOSMJSON.load(content, {}, (err, result) => {
+        parsedData = result
+
+        done(err)
+      })
+    })
+  })
+
+  it ('.osm', function (done) {
+    loadFile('test/small.osm', (err, content) => {
+      fileFormatOSMXML.load(content, {}, (err, result) => {
+        if (err) {
+          return done(err)
+        }
+
+        assert.deepEqual(result, parsedData)
+
+        done()
+      })
+    })
+  })
+
+  it ('.osm.bz2', function (done) {
+    loadFile('test/small.osm.bz2', (err, content) => {
+      fileFormatOSMXML.load(content, {}, (err, result) => {
+        if (err) {
+          return done(err)
+        }
+
+        assert.deepEqual(result, parsedData)
+
+        done()
+      })
+    })
+  })
+})
