58c1408d9205d3268372bd731f55e83666bdb47e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-05-15 14:17:22 +0200

    fileFormatGeoJSON: copy meta data (generator, copyright, ...)

--- src/fileFormatGeoJSON.js
+++ src/fileFormatGeoJSON.js
@@ -11,9 +11,25 @@ module.exports = {
     const data = JSON.parse(content)
 
     const result = {
-      version: 0.6,
-      elements: []
+      version: 0.6
     }
+
+    ;['generator', 'timestamp_osm_base', 'copyright'].forEach(k => {
+      if (data[k]) {
+        result[k] = data[k]
+      }
+    })
+
+    if (data.bounds) {
+      result.bounds = {
+        minlon: data.bounds[0],
+        minlat: data.bounds[1],
+        maxlon: data.bounds[2],
+        maxlat: data.bounds[3]
+      }
+    }
+
+    result.elements = []
 
     try {
       geojson2elements(data, result.elements, options)
