3b3e8a4d11ca04304897b76a73c91d78211982b7
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-06-27 16:53:54 +0200

    DBTypeOverpassAPI: move code from RequestBBox

--- src/DBTypeOverpassAPI.js
+++ src/DBTypeOverpassAPI.js
@@ -1,8 +1,50 @@
 const httpLoad = require('./httpLoad')
 const boundsIsFullWorld = require('./boundsIsFullWorld')
+const overpassOutOptions = require('./overpassOutOptions')
 const DBTypeBase = require('./DBTypeBase')
 
 module.exports = class DBTypeOverpassAPI extends DBTypeBase {
+  compile (query, options) {
+    // let query;
+    let resultSet = '.result'
+
+    // if the context already has a bbox and it differs from this, we can't add
+    // ours
+    if (this.lokiQuery) {
+      query = this.lokiQuery.toQl({ setsUseStatementIds: true }) + '\n'
+      this.options.properties |= this.lokiQuery.properties()
+      resultSet = options.statementId ? '._' + options.statementId : '.result'
+    } else {
+      query = this.query.substr(0, this.query.length - 1) + '->.result;\n'
+    }
+
+    let queryRemoveDoneFeatures = ''
+    let countRemoveDoneFeatures = 0
+    for (const id in this.doneFeatures) {
+      const ob = this.doneFeatures[id]
+
+      if (countRemoveDoneFeatures % 1000 === 999) {
+        query += '(' + queryRemoveDoneFeatures + ')->.done;\n'
+        queryRemoveDoneFeatures = '.done;'
+      }
+
+      queryRemoveDoneFeatures += ob.type + '(' + ob.osm_id + ');'
+      countRemoveDoneFeatures++
+    }
+
+    if (countRemoveDoneFeatures) {
+      query += '(' + queryRemoveDoneFeatures + ')->.done;\n'
+      query += '(' + resultSet + '; - .done;)->' + resultSet + ';\n'
+    }
+
+    if (!('split' in this.options)) {
+      this.options.effortSplit = Math.ceil(options.effortAvailable / this.overpass.options.effortBBoxFeature)
+    }
+    query += resultSet + ' out ' + overpassOutOptions(this.options) + ';\n'
+
+    return query
+  }
+
   execute (context, callback) {
     let queryOptions = ''
 
