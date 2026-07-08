7dd13dd4362e37f1381f9fb312a3244f2a072922
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2026-07-08 20:57:21 +0200

    DB OverpassAPI: create execute() from GeowikiAPI code

--- src/DBTypeOverpassAPI.js
+++ src/DBTypeOverpassAPI.js
@@ -1,4 +1,24 @@
+const httpLoad = require('./httpLoad')
+const boundsIsFullWorld = require('./boundsIsFullWorld')
 const DBTypeBase = require('./DBTypeBase')
 
 module.exports = class DBTypeOverpassAPI extends DBTypeBase {
+  execute (context, callback) {
+    let queryOptions = ''
+
+    queryOptions = '[out:json]'
+    if (context.bbox && !boundsIsFullWorld(context.bbox)) {
+      queryOptions += '[bbox:' + context.bbox.toLatLonString() + ']'
+    }
+
+    const query = queryOptions + ';\n' +
+      context.subRequests.map(c => c.query).join('\nout count;\n')
+
+    httpLoad(
+      this.url,
+      null,
+      query,
+      callback
+    )
+  }
 }
